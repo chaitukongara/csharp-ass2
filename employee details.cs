81498dc2d5349e2c28c915a31e865f8fb7708ae3
chaitukongara <121846773+chaitukongara@users.noreply.github.com>
2023-01-25 12:34:41 +0530

    Add files via upload

---
+++ employee details.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EmployeeDetails;
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Class1 c1 = new Class1();
+                Console.WriteLine("Enter Employee id:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Employee salary:");
+                double sal = Convert.ToDouble(Console.ReadLine());
+
+                c1.EmpNo = id;
+                c1.EmpName = name;
+                c1.Salary = sal;
+                c1.CalculateSalary();
+                Console.WriteLine("Employee Grosssalary:{0}", c1.GrossSalary);
+                Console.WriteLine("Employee Hra:{0}", c1.HRA);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ litware.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace EmployeeDetails
+{
+	public class Class1
+	{
+		private int _empNo;
+		private string _empName;
+		private double _salary;
+		private double _hra;
+		private double _ta;
+		private double _da;
+		public double PF;
+		public double TDS;
+		public double NetSalary;
+		private double _grossSalary;
+		public int EmpNo
+		{
+			get
+			{
+				return _empNo;
+			}
+			set
+			{
+				_empNo = value;
+			}
+		}
+
+		public string EmpName
+		{
+			get
+			{
+				return _empName;
+			}
+			set
+			{
+				_empName = value;
+			}
+		}
+		public double Salary
+		{
+			get
+			{
+
+				return _salary;
+			}
+			set
+			{
+				_salary = value;
+			}
+		}
+
+		public double HRA
+		{
+			get
+			{
+				if (_salary < 5000)
+				{
+					_hra = 0.1 * _salary;
+
+				}
+				else if (_salary < 10000)
+				{
+					_hra = 0.15 * _salary;
+				}
+				else if (_salary < 15000)
+				{
+					_hra = 0.2 * _salary;
+				}
+				else if (_salary < 20000)
+				{
+					_hra = 0.25 * _salary;
+				}
+				else if (_salary >= 20000)
+				{
+					_hra = 0.3 * _salary;
+				}
+				return _hra;
+			}
+		}
+		public double Ta
+		{
+			get
+			{
+				if (_salary < 5000)
+				{
+					_ta = 0.05 * _salary;
+
+				}
+				else if (_salary < 10000)
+				{
+					_ta = 0.1 * _salary;
+				}
+				else if (_salary < 15000)
+				{
+					_ta = 0.15 * _salary;
+				}
+				else if (_salary < 20000)
+				{
+					_ta = 0.2 * _salary;
+				}
+				else if (_salary >= 20000)
+				{
+					_ta = 0.25 * _salary;
+				}
+				return _ta;
+			}
+		}
+		public double Da
+		{
+			get
+			{
+				if (_salary < 5000)
+				{
+					_da = 0.15 * _salary;
+
+				}
+				else if (_salary < 10000)
+				{
+					_da = 0.2 * _salary;
+				}
+				else if (_salary < 15000)
+				{
+					_da = 0.25 * _salary;
+				}
+				else if (_salary < 20000)
+				{
+					_da = 0.3 * _salary;
+				}
+				else if (_salary >= 20000)
+				{
+					_da = 0.35 * _salary;
+				}
+				return _da;
+			}
+		}
+		public double GrossSalary
+		{
+			get
+			{
+				return _grossSalary = _salary + _hra + _ta + _da;
+
+			}
+		}
+		public void CalculateSalary()
+		{
+			PF = 0.1 * GrossSalary;
+			Console.WriteLine("PF:{0}", PF);
+			TDS = 0.18 * GrossSalary;
+			Console.WriteLine("TDS:{0}", TDS);
+			NetSalary = GrossSalary - (PF + TDS);
+			Console.WriteLine("NET Salary:{0}", NetSalary);
+
+
+		}
+
+	}
+}
